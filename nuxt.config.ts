e86820c585abd02d67c6242e09f0bc3fc28483c0
Jed Bartolo <jedbartolo11@gmail.com>
2024-10-06 15:20:25 +0800

    Use typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+
+import type { NuxtConfig } from "@nuxt/types";
+
+const config: NuxtConfig = {
+  buildModules: ["@nuxt/typescript-build"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: [
     "@fortawesome/fontawesome-svg-core/styles.css",
     "~/assets/scss/main.scss",
   ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
   build: {
     transpile: ["@fortawesome/vue-fontawesome"],
   },
-});
+};
+
+export default config;
