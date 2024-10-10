d75042d1abc73fc775830ad0d5f2df0c98170e02
Jed Bartolo <jedbartolo11@gmail.com>
2024-10-10 20:39:32 +0800

    Card Component

---
+++ components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <article
+        class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-rounded tw-bg-lightgray tw-px-4 tw-py-8"
+    >
+        <slot></slot>
+        <h2 class="tw-mb-2 tw-uppercase">{{ title }}</h2>
+        <p class="tw-mb-2 tw-text-center tw-font-light">{{ description }}</p>
+        <NuxtLink class="tw-uppercase" v-if="url" :to="'#'">{{
+            urlLabel
+        }}</NuxtLink>
+    </article>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+    title: string;
+    description: string;
+    url?: string;
+    urlLabel?: string;
+}
+
+const props = defineProps<{ props: Props }>();
+const { title, description, url, urlLabel } = props?.props;
+</script>
