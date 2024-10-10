72b448e4002b651f0d4ca3f71f9f866467792d2f
Jed Bartolo <jedbartolo11@gmail.com>
2024-10-10 20:34:26 +0800

    Update main fonts and color schemes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  prefix: "tw-",
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
-  theme: {
-    colors: {
-      main: "#5645ff",
-      dark: "#242121",
+    prefix: "tw-",
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Josefin Sans", "sans-serif"],
+        },
+        extend: {
+            colors: {
+                main: "#5645ff",
+                dark: "#242121",
+                lightgray: "#f8f8f8",
+            },
+        },
     },
-    fontFamily: {
-      sans: ["Josefin Sans", "sans-serif"],
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
