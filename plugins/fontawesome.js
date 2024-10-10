0c8bbff8611451ba294dacc3f2d4b98c78fab716
Jed Bartolo <jedbartolo11@gmail.com>
2024-10-10 20:39:44 +0800

    Fontawesome icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,41 +1,53 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import {
-  faBuromobelexperte,
-  faDeezer,
+    faBuromobelexperte,
+    faDeezer,
+    faFacebook,
+    faXTwitter,
+    faGooglePlusG,
+    faDribbble,
+    faPinterest,
 } from "@fortawesome/free-brands-svg-icons";
+
 import { faFaceSmileBeam } from "@fortawesome/free-regular-svg-icons";
 
 import {
-  faBars,
-  faMagnifyingGlass,
-  faXmark,
-  faBezierCurve,
-  faPenNib,
-  faFont,
-  faLayerGroup,
-  faAward,
-  faChampagneGlasses,
+    faBars,
+    faMagnifyingGlass,
+    faXmark,
+    faBezierCurve,
+    faPenNib,
+    faFont,
+    faLayerGroup,
+    faAward,
+    faChampagneGlasses,
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-  faBars,
-  faMagnifyingGlass,
-  faXmark,
-  faBuromobelexperte,
-  faBezierCurve,
-  faPenNib,
-  faFont,
-  faLayerGroup,
-  faDeezer,
-  faFaceSmileBeam,
-  faAward,
-  faChampagneGlasses
+    faBars,
+    faMagnifyingGlass,
+    faXmark,
+    faBuromobelexperte,
+    faBezierCurve,
+    faPenNib,
+    faFont,
+    faLayerGroup,
+    faDeezer,
+    faFaceSmileBeam,
+    faAward,
+    faChampagneGlasses,
+    faFacebook,
+    faXTwitter,
+    faGooglePlusG,
+    faDribbble,
+    faPinterest,
 );
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon, {});
+    nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon, {});
 });
